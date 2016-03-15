314d4db6d41b7afdbec630cef7fc5d81483188a2
JakeQiu3 <qsy118614@126.com>
2016-03-15 19:10:42 +0800

    小修改

--- MemoryControl/FirstViewController.h
+++ MemoryControl/FirstViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface FirstViewController : UIViewController
+//文／汉斯哈哈哈（简书作者）
 
+// weak:指明该对象并不负责保持delegate这个对象，delegate这个对象的销毁由外部控制。
+
+// strong：该对象强引用delegate，外界不能销毁delegate对象，会导致循环引用(Retain Cycles)
 @end
